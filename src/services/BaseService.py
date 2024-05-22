2ac65704d3261f80d5011c74d0b8ae92fbb95621
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2024-05-22 23:44:19 +0200

    Delete: Eliminación de archivos innecesarios por duplicidad de contenido

--- src/services/BaseService.py
+++
@@ -1,9 +0,0 @@
-from utils.Utils import Utils
-
-class BaseService:
-    def manage_response(self, question) -> str:
-        response = Utils.ask_to_the_llama(message=question)
-        
-        # Mandar esto a una base de datos
-        
-        return response['message']['content']
