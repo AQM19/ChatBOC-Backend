637cdbe88fb582635cf28b2cdde45fb06eea9cc9
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2024-05-22 23:43:48 +0200

    Refactor: Cambio de nombre y funcionalidad al script de inicio

--- initDockers.sh
+++
@@ -1,5 +0,0 @@
-conda deactivate
-source chatboc-env/bin/activate
-docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-docker pull postgres
-docker run --name chatboc-db -e POSTGRES_PASSWORD=@1Xygm352Z+chatboc -d -p 5432:5432 postgres
