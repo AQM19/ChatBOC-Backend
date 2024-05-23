592e4d3b7c669920b25b2877aec0cdc44fd7ce8d
rumantela <adolfosanchezlopez@gmail.com>
2024-05-23 06:52:36 +0200

    Feat: __init__

--- src/api/ollama/ollamaModel.py
+++ src/api/ollama/ollamaModel.py
@@ -1,9 +1,12 @@
 import ollama
 import markdown
-
+from utils.driverDB import DB
+user_id = "2f86408f-e2f5-45f7-b9df-58c65f7ce252"
+chat_id = "039acaf2-8c29-4857-a194-7d68de5a4052"
 while(True):
   question = input("\nTu pregunta: ")
   response = ""
+  chunks = []
   if question == "/exit":
     print("Hasta pronto!")
     break
@@ -13,8 +16,16 @@ while(True):
       stream=True,
   )
   print("Respondiendo a tu pregunta: ", question)
+  db = DB(user_id)
+  #chat_id = db.getChatID(db.getUserID())
+  #db.getStream(stream, chat_id)
+
   for chunk in stream:
+    # print(chunk)
     print(chunk['message']['content'], end='', flush=True)
     response += chunk['message']['content']
+    chunks.append(chunk)
   response_html = markdown.markdown(response)
   print(response_html)
+  db.saveChunks(chat_id, user_id, chunks)
+  
