306fc330d68c70d59790b48814da0dd647368317
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2024-05-30 20:02:02 +0200

    Refactor: Movido el cronjob a la carpeta de scripts

---
+++ src/scripts/cronjob.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Recordad cambiar en constantes los path de los directorios. Logs y pdfs por separado
+#* 6 * * * /path/cronjob.sh path al cronjob
+/path/F-ChatBOC-develop/.venv/bin/python /path/F-ChatBOC-develop/scraper/main.py
