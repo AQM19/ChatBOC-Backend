7a85b642114dec72a367ff75eef63ccbc7dee791
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2024-05-30 19:13:44 +0200

    Feat: Adición del template del preprompt

--- utils/Utils.py
+++ utils/Utils.py
@@ -2,21 +2,30 @@ import ollama
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 
+
 class Utils:
-    
+
     @staticmethod
     def ask_to_the_llama(message):
+
+        pre_prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>Eres una asistente para respuesta de preguntas.
+            Usa el contexto proporcionado para contestar a la pregunta. Si no conoces la respuesta, di que no conoces la respuesta.
+            Responde siempre en español y con 4 frases como mucho. <|eot_id|><|start_header_id|>user<|end_header_id|>
+            Question: {question}
+            Context: {context}
+            Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+
         response = ollama.chat(model='llama3', messages=[
             {
                 'role': 'user',
                 'content': message,
             },
         ])
-        
+
         return response
-    
+
     @staticmethod
-    def transformpdf(path,chunk_size=1024,chunk_overlap=128):
+    def transformpdf(path, chunk_size=1024, chunk_overlap=128):
         """
         Transforma un archivo PDF en fragmentos de texto.
 
@@ -30,7 +39,8 @@ class Utils:
         """
 
         data = PyPDFLoader(path)
-        text_splitter=CharacterTextSplitter(separator='\n', length_function=len,chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        text_splitter = CharacterTextSplitter(
+            separator='\n', length_function=len, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         splits = text_splitter.split_documents(data.load())
 
         return splits
