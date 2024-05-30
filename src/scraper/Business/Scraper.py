e5510b823ab84d53f0afc22bb222f3da699c4a76
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2024-05-30 22:38:34 +0200

    Refactor: Arreglo de la clase Scrapper

--- src/scraper/Business/Scraper.py
+++ src/scraper/Business/Scraper.py
@@ -1,3 +1,5 @@
+from typing import Any, Literal
+from urllib import response
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from multiprocessing.pool import ThreadPool
@@ -6,27 +8,28 @@ import logging
 import os
 import requests
 
+
 class Scraper:
 
     def __init__(self) -> None:
         load_dotenv()
-        self.docsCheckpoint = []
-        self.boc = os.getenv('BOC')
-        self.docs_path = os.getenv('DOCS_PATH')
-        self.pdfs_path = os.getenv('PDFS_PATH')
-        self.patience = os.getenv('PATIENCE')
-        self.num_iter_max = os.getenv('NUM_ITER_MAX')
-        self.download_from = os.getenv('DOWNLOAD_FROM')
-        self.num_processes = os.getenv('NUM_PROCESSES')
-        
-        #Arrancamos logger
-        self.logger = logging.getLogger(__name__)
-        
+        self.docsCheckpoint: list[int] = []
+        self.boc: str = os.getenv('BOC')
+        self.docs_path: str = os.getenv('DOCS_PATH')
+        self.pdfs_path: str = os.getenv('PDFS_PATH')
+        self.patience: int = int(os.getenv('PATIENCE'))
+        self.num_iter_max: int = int(os.getenv('NUM_ITER_MAX'))
+        self.download_from: int = int(os.getenv('DOWNLOAD_FROM'))
+        self.num_processes: int = int(os.getenv('NUM_PROCESSES'))
+
+        # Arrancamos logger
+        self.logger: logging.Logger = logging.getLogger(__name__)
+
         self.init()
-    
+
     def init(self):
-        #Comprobamos si la carpeta de descarga esta creada
-        check_folder = os.path.isdir(self.docs_path)
+        # Comprobamos si la carpeta de descarga esta creada
+        check_folder: bool = os.path.isdir(self.docs_path)
 
         if not check_folder:
             os.makedirs(self.pdfs_path)
@@ -34,96 +37,103 @@ class Scraper:
 
         else:
             self.logger.info(f"> {self.pdfs_path} ya existe.")
-        
-        #Leemos cuantos pdfs hay en el directorio en caso de corte o problema 
+
+        # Leemos cuantos pdfs hay en el directorio en caso de corte o problema
         if len(os.listdir(self.pdfs_path)) == 0:
             self.logger.warn("? - No se encontraron documentos descargados.")
             self.logger.warn("? - La descarga comenzará desde cero.")
 
         else:
-            #Con esto recogemos el id de los pdfs que ya tenemos descargados
-            self.logger.warn("? - Se encontraron documentos ya descargados... ")
+            # Con esto recogemos el id de los pdfs que ya tenemos descargados
+            self.logger.warn(
+                "? - Se encontraron documentos ya descargados... ")
             for path in os.listdir(self.pdfs_path):
                 if os.path.isfile(os.path.join(self.pdfs_path, path)):
                     self.docsCheckpoint.append(int(path.split(".")[0]))
 
     def scrape_All_PDFs(self):
 
-        validUrls=[]
-        patience = 0
+        validUrls: list = []
+        patience: int = 0
 
-        for i in tqdm(range(self.download_from,self.num_iter_max)):
+        for i in tqdm(range(self.download_from, self.num_iter_max)):
 
-                if i not in self.docsCheckpoint:
-                    testUrl=self.get_Valid_Urls(f"{self.boc}{i+1}")
+            if i not in self.docsCheckpoint:
+                testUrl: (tuple[Any, Literal[False]]
+                          | tuple[Any, Literal[True]]) = self.get_Valid_Urls(f"{self.boc}{i+1}")
+                url: (tuple[Any, Literal[False]] |
+                      tuple[Any, Literal[True]]) = testUrl[0]
+                error: (tuple[Any, Literal[False]] |
+                        tuple[Any, Literal[True]]) = testUrl[1]
 
-                    url = testUrl[0]
-                    error = testUrl[1]
-                    
-                    if not error:
-                        validUrls.append(url)
+                if not error:
+                    validUrls.append(url)
 
-                    elif patience == self.patience:
-                        self.logger.info("? Se supero la paciencia")
-                        break
+                elif patience == self.patience:
+                    self.logger.info("? Se supero la paciencia")
+                    break
 
-                    else:
-                        self.logger.warn(f"? Se encontró un error en {url}. Probablemente no exista.")
-                        patience += 1
-                        self.logger.warn(f"? Paciencia {patience}/{self.patience}")
-
-        
+                else:
+                    self.logger.warn(
+                        f"? Se encontró un error en {url}. Probablemente no exista.")
+                    patience += 1
+                    self.logger.warn(f"? Paciencia {patience}/{self.patience}")
 
         self.logger.info("> Iniciando descarga del BOC...")
-        
-        pool=ThreadPool(processes=self.num_processes)
-        pool.map(self.download_PDF,validUrls)
+
+        pool: ThreadPool = ThreadPool(processes=self.num_processes)
+        pool.map(self.download_PDF, validUrls)
         pool.close()
         pool.join()
 
-    def get_Valid_Urls(self,url):
+    def get_Valid_Urls(self, url):
 
         try:
-            #Aquí le estamos diciendo que pruebe desde el último documento recuperado
-            #hasta el número maximo de iteraciones seleccionado en constantes.
-            
-            headers = ""
-            proxy = ""
+            # Aquí le estamos diciendo que pruebe desde el último documento recuperado
+            # hasta el número maximo de iteraciones seleccionado en constantes.
+
+            headers: str = ""
+            proxy: str = ""
 
             self.logger.info(f"> Comprobando {url}")
 
-            #Hacemos la llamada, comprobamos si existe la url y si hay pdf
-            r = requests.get(f"{url}", headers=headers,proxies=proxy)
+            # Hacemos la llamada, comprobamos si existe la url y si hay pdf
+            r: response = requests.get(
+                f"{url}", headers=headers, proxies=proxy)
 
-            soup = BeautifulSoup(r.text, 'html.parser')
+            soup: BeautifulSoup = BeautifulSoup(r.text, 'html.parser')
 
             # Solo por el status code no podemos saber si existen documentos, asi que tenemos que buscarlo y tenerlo en cuenta
             error = soup.find('div', id='errorDocumento')
 
             if r.status_code == 200 and error is None:
-                self.logger.info( f"? - La url {url} es valida :). ")
-                return url,False
-            
+                self.logger.info(f"? - La url {url} es valida :). ")
+                return url, False
+
             else:
-                self.logger.warn( f"? - La url {url} devolvió un status code de : {r.status_code}. Div element -> {error.text}")
-                return url,True
+                self.logger.warn(
+                    f"? - La url {url} devolvió un status code de : {r.status_code}. Div element -> {error.text}")
+                return url, True
 
         except Exception as e:
-            self.logger.error(f"X - Se ha encontrado un error en la comprobación de la url {url}.")
-            self.logger.error( f"? - La url {url} devolvió un status code de : {r.status_code}. ")
-            return url,True
+            self.logger.error(
+                f"X - Se ha encontrado un error en la comprobación de la url {url}.")
+            self.logger.error(
+                f"? - La url {url} devolvió un status code de : {r.status_code}. ")
+            return url, True
 
-    def download_PDF (self,url):
-        headers = ""
-        proxy = ""
+    def download_PDF(self, url):
+        headers: str = ""
+        proxy: str = ""
 
-        r = requests.get(f"{url}", stream = True,headers=headers,proxies=proxy)
+        r: response = requests.get(
+            f"{url}", stream=True, headers=headers, proxies=proxy)
 
-        #Recogemos el id del fichero
-        id = url.split("=")[1]
-        pdfPath = f"{self.pdfs_path}/{id}"
+        # Recogemos el id del fichero
+        id: Any = url.split("=")[1]
+        pdfPath: str = f"{self.pdfs_path}/{id}"
 
-        with open(f"{pdfPath}.pdf", 'wb') as f:           
+        with open(f"{pdfPath}.pdf", 'wb') as f:
             f.write(r.content)
-        
+
         self.logger.info(f"> Documento .pdf {id} descargado.")
--- src/scraper/main.py
+++ src/scraper/main.py
@@ -7,25 +7,25 @@ from Business.Scraper import Scraper
 
 def main():
     load_dotenv()
-    pdfs_path = os.getenv('PDFS_PATH')
+    logs_path: str = os.getenv('LOGS_PATH')
 
     # Comprobamos si la carpeta de logs esta creada
-    CHECK_LOGS_FOLDER = os.path.isdir(pdfs_path)
+    check_logs_folder: bool = os.path.isdir(logs_path)
 
     # Si la carpeta no existe la crea
-    if not CHECK_LOGS_FOLDER:
-        os.makedirs(pdfs_path)
+    if not check_logs_folder:
+        os.makedirs(logs_path)
 
     # Fecha para organizar los logs
-    date = datetime.now()
-    format_date = str(date.year) + str(date.month).zfill(2) + \
+    date: datetime = datetime.now()
+    format_date: str = str(date.year) + str(date.month).zfill(2) + \
         str(date.day).zfill(2)
 
     # Arrancamos el logger
-    logging.basicConfig(filename=f"{pdfs_path}/scraper-{format_date}.log", level=logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    logging.basicConfig(filename=f"{logs_path}/scraper-{format_date}.log", level=logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    s = Scraper()
+    s: Scraper = Scraper()
     s.scrape_All_PDFs()
 
 
