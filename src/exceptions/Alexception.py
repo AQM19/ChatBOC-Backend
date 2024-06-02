3fb2c5bcb198b91f3a3f4ac3198e2a1ea447911a
AQM19 <quintanalaaron@gmail.com>
2024-06-02 20:23:23 +0200

    Huevo de pascua

---
+++ src/exceptions/Alexception.py
@@ -0,0 +1,15 @@
+class EasterEggsception(Exception):
+
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
+
+    def cafe_cuarentona(iosu):
+        if iosu:
+            print('Descansando')
+            return
+        
+        print('Descansando igualmente')
+
+    def matrix():
+        print('¡Conectadme a la matrix!')
